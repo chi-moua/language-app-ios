6a24a30d539bf7c5c5353f97b7967c3313ef540f
chi-moua <kongfuechimoua@gmail.com>
2022-05-18 18:10:22 -0500

    initial project

---
+++ LangApp/LangApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LangApp
+//
+//  Created by Kongfuechi Moua on 5/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
