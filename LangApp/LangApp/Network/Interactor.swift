687f28bc9bdc588e3ee26c22c65bd1ed6751dd08
chi-moua <kongfuechimoua@gmail.com>
2022-06-17 20:42:09 -0500

    adding initial interactor file

--- LangApp/LangApp/Network/Interactor.swift
+++ LangApp/LangApp/Network/Interactor.swift
@@ -5,4 +5,41 @@
 //  Created by Kongfuechi Moua on 6/17/22.
 //
 
+import ReSwift
 import Foundation
+
+class Interactor: StoreSubscriber {
+    let store: Store<State>
+    // Service class go in here
+    
+    init(store: Store<State>) {
+        self.store = store
+    }
+    
+    private func subscribe() {
+        //subscribe to execution state
+    }
+    
+    func newState(state: State.NetworkState.ExecutionState?) {
+        guard case let .start(request) = state else {
+            return
+        }
+        
+        //dispatch loading
+        
+        //execute request
+        
+        //dispatch to store .complete(result) or .error(result)
+    }
+}
+
+extension Interactor {
+    enum Request: Equatable {
+        case news
+    }
+    
+    enum Action: AppAction, Equatable {
+        case loading //update execution state
+        case complete // handle response
+    }
+}
