4607b6ea161f79d72ce79310745d92ef9a98a82c
chi-moua <kongfuechimoua@gmail.com>
2022-08-06 13:32:52 -0500

    update tab bar to show VideoChannelPicker as intial screen

--- LangApp/LangApp/ViewController/TabBarViewController.swift
+++ LangApp/LangApp/ViewController/TabBarViewController.swift
@@ -51,7 +51,7 @@ extension TabBarViewController {
 extension TabBarViewController {
     private func setupViewControllers() {
         let newsTab = NewsViewController(store: store)
-        let videoTab = VideoChannelPicker()
+        let videoTab = VideoChannelPickerViewController()
         let podcastTab = PodcastViewController()
         let musicTab = MusicViewController()
 
